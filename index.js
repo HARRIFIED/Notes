52f8920aa9153a05c66cf0d98a3098dbb91aa4c3
HP <harrivic400@gmail.com>
2024-07-25 02:09:18 +0100

    Connect Database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+require("./server/config/db")();
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
+
 
 const app = express();
 const port = 5000 || process.env.PORT;
@@ -10,6 +12,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 
+//Connect DB
+// connectDB()
 //Template
 app.use(expressLayouts);
 app.set('layout', './layouts/main');
@@ -19,9 +23,14 @@ app.set('view engine', 'ejs')
 app.use('/', require('./server/routes/index'))
 app.use('/', require('./server/routes/dashboard'))
 
-app.get('*', function (req, res) {
-    res.status(404).render('404')
-})
+//Page not found
+app.use("*", (req, res) => {
+    try {
+        return res.redirect("/")
+    } catch (err) {
+        return res.redirect("/");
+    }
+});
 
 app.listen(port, () => {
     console.log(`server running at port ${port}`)
---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`Database Connected: ${connect.connection.host}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+module.exports = connectDB;
