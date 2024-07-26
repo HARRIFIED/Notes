44308691f9eeb5ada13eee46d7fff3c68581a432
HP <harrivic400@gmail.com>
2024-07-26 13:51:20 +0100

    [ADD] Add google auth, model & middleware

--- index.js
+++ index.js
@@ -2,24 +2,39 @@ require('dotenv').config();
 require("./server/config/db")();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const passport = require("passport");
 
 
 const app = express();
 const port = 5000 || process.env.PORT;
+app.use(session({
+    secret: 'Hello World',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URL
+    })
+}));
+
 
 //Middlewwares
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 
-//Connect DB
-// connectDB()
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Template
 app.use(expressLayouts);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs')
 
 //routing
+app.use('/', require('./server/routes/auth'))
 app.use('/', require('./server/routes/index'))
 app.use('/', require('./server/routes/dashboard'))
 
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require("../controllers/dashboardController");
+const { isLoggedIn } = require('../middleware/checkAuth')
 
 //Routes
-router.get('/dashboard', dashboardController.homepage);
+router.get('/dashboard', isLoggedIn, dashboardController.homepage);
 
 
 
